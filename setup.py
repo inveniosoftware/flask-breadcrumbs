5c1d454801b553b9a4ddf9d485e2a279a699abab
Tibor Simko <tibor.simko@cern.ch>
2014-08-05 17:30:36 +0200

    global: license change to Revised BSD License
    * The Flask-Breadcrumbs extension is now released under more permissive
      Revised BSD License.
    
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- setup.py
+++ setup.py
@@ -4,22 +4,8 @@
 ## Copyright (C) 2013, 2014 CERN.
 ##
 ## Flask-Breadcrumbs is free software; you can redistribute it and/or
-## modify it under the terms of the GNU General Public License as
-## published by the Free Software Foundation; either version 2 of the
-## License, or (at your option) any later version.
-##
-## Flask-Breadcrumbs is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-## General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with Flask-Breadcrumbs; if not, write to the Free Software Foundation,
-## Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-##
-## In applying this licence, CERN does not waive the privileges and immunities
-## granted to it by virtue of its status as an Intergovernmental Organization
-## or submit itself to any jurisdiction.
+## modify it under the terms of the Revised BSD License; see LICENSE
+## file for more details.
 
 from setuptools import setup
 
@@ -27,7 +13,7 @@ setup(
     name='Flask-Breadcrumbs',
     version='0.1.1.dev20140801',
     url='http://github.com/inveniosoftware/flask-breadcrumbs/',
-    license='GPLv2',
+    license='BSD',
     author='Invenio collaboration',
     author_email='info@invenio-software.org',
     description='Flask-Breadcrumbs is a Flask extension that adds support '
@@ -45,7 +31,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Flask',
